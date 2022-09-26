405d9a5f719aacd4e9708eff976e5c108f0ee0dd
TheLucas27 <Lucemi_cheung@hotmail.com>
2022-09-27 00:19:45 +0200

    finished assignment 3
    Finished assignment 3.
    Added more comments to assignment 1, 2 and 3

--- Week 4/Iterationassignments/assignment2/Program.cs
+++ Week 4/Iterationassignments/assignment2/Program.cs
@@ -13,15 +13,17 @@
             int randomNumber = int.Parse(Console.ReadLine());
             int numberCount = 0;
 
+            // calculate number count
             do
-
             {
+                // if input = target number
                 if (targetNumber == randomNumber)
                 {
                     Console.Write("Enter a number :");
                     randomNumber = int.Parse(Console.ReadLine());
                     numberCount++;
                 }
+                // if input != target number
                 else if (targetNumber != randomNumber && randomNumber != 0)
                 {
                     Console.Write("Enter a number :");
@@ -30,7 +32,7 @@
             } while (randomNumber != 0);
 
             
-
+            // display number count
             Console.Write($"Count of numbers equal to target number: {numberCount}");
         }
     }
--- Week 4/Iterationassignments/assignment3/Program.cs
+++ Week 4/Iterationassignments/assignment3/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // initialise values
+            int randomNumber = 0;
+            int consoleWrite = 0;
+            int sum = 0;
+
+            // calculate every 5th input
+            do
+            {
+                consoleWrite++;
+                // if it's every 5th input
+                if (consoleWrite % 5 == 0)
+                {
+                    Console.Write("Enter a number: ");
+                    randomNumber = int.Parse(Console.ReadLine());
+                    sum += randomNumber;
+                }
+                // if it's not every 5th input
+                else if (consoleWrite % 5 != 0)
+                {
+                    Console.Write("Enter a number: ");
+                    randomNumber = int.Parse(Console.ReadLine());
+                }
+            } while (randomNumber != 0);
+
+            // display sum
+            Console.Write($"Sum of 5th, 10th, 15th ... number is: {sum} ");
+
         }
     }
 }
