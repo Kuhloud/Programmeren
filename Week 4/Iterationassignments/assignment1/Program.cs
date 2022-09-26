6409283a1fb825e199e74c55e01c38095593a11a
TheLucas27 <Lucemi_cheung@hotmail.com>
2022-09-26 12:53:45 +0200

    renamed
    renamed iterationassignment to assignment 1

---
+++ Week 4/Iterationassignments/assignment1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // display value
+            Console.WriteLine("Enter a number: ");
+            int randomValue = int.Parse(Console.ReadLine());
+
+            while (randomValue != 0)
+            {
+                Console.WriteLine("Enter a number: ");
+                randomValue = int.Parse(Console.ReadLine());
+            }
+            Console.Write("Average of all positive numbers is: ");
+        }
+    }
+}
