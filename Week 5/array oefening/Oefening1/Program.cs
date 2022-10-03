01b32636e11e0ded344f84f69895666b8faabf86
TheLucas27 <Lucemi_cheung@hotmail.com>
2022-10-03 16:49:25 +0200

    practice

---
+++ Week 5/array oefening/Oefening1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Xml.Schema;
+
+namespace Oefening1
+{
+    internal class Program
+    {
+        const int MonthsInAYear = 12;
+        static void Main(string[] args)
+        {
+            string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+            int[] profit = new int[MonthsInAYear];
+
+            int total = 0;
+            for (int i = 0; i < profit.Length; i++)
+            {
+                Console.Write($"Enter profit for {months[i]}: ");
+                profit[i] = int.Parse(Console.ReadLine());
+                total += profit[i];  
+            }
+             double average = total / profit.Length;
+
+            for (int i = 0; i < profit.Length; i++)
+            {
+                if (profit[i] > average)
+                {
+                    string result = months[i];
+                    Console.WriteLine($"{result}. {profit[i]}.");
+                }
+            }
+        }
+    }
+}
---
+++ Week 5/array oefening/Oefening2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Oefening2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int[] numbers = new int[6];
+
+            for (int i = 0; i < numbers.Length; i++);
+            {
+                numbers[1] = random.Next(1, 11);
+            }
+        }
+    }
+}
