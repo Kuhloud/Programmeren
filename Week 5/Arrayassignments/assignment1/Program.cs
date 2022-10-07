f1a9c2b0d093bacf86cecc16a2bd0236ba22d2d9
TheLucas27 <Lucemi_cheung@hotmail.com>
2022-10-07 17:08:24 +0200

    Finished assignment 1
    took me like 3 hours

--- Week 5/Arrayassignments/assignment1/Program.cs
+++ Week 5/Arrayassignments/assignment1/Program.cs
@@ -2,9 +2,41 @@
 {
     internal class Program
     {
+        const int FixedAmountOfElements = 20;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] element = new int[FixedAmountOfElements];
+            int consoleWriteLine = 0;
+            int elementSum = 0;
+
+            for (int i = 0; i < element.Length; i++)
+            {
+                consoleWriteLine++;
+                element[i] = new Random().Next(0, 200);
+                Console.WriteLine($"Element {i} is {element[i]}");
+                elementSum += element[i];
+            }
+            double average = (double)elementSum / consoleWriteLine;
+            Console.WriteLine($"\nThe Average is {average:0.00}\n");
+            for (int i = 0; i < element.Length; i++)
+            {
+                if (average <= element[i])
+                {
+                    double difference = (double)element[i] - average;
+                    Console.WriteLine($"Difference between average and element {i} is {difference:0.00}");
+                }
+                else if (average >= element[i])
+                {
+                    double difference = average - (double)element[i];
+                    Console.WriteLine($"Difference between average and element {i} is {difference:0.00}");
+                }
+            }
+
+
+
+
+
+
         }
     }
 }
