6c0873faf039b35661bc1587149efb188968e57d
TheLucas27 <Lucemi_cheung@hotmail.com>
2022-10-07 19:55:37 +0200

    Finished assigment 2

--- Week 5/Arrayassignments/assignment1/Program.cs
+++ Week 5/Arrayassignments/assignment1/Program.cs
@@ -1,23 +1,35 @@
-﻿namespace assignment1
+﻿using System.Globalization;
+
+namespace assignment1
 {
     internal class Program
     {
         const int FixedAmountOfElements = 20;
         static void Main(string[] args)
         {
+            // set culture program
+            CultureInfo ci = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+
+            // initialize values
             int[] element = new int[FixedAmountOfElements];
             int consoleWriteLine = 0;
             int elementSum = 0;
 
-            for (int i = 0; i < element.Length; i++)
+            // display values
+            for (int i = 0; i < element.Length; i++) // display 20 elements
             {
                 consoleWriteLine++;
                 element[i] = new Random().Next(0, 200);
                 Console.WriteLine($"Element {i} is {element[i]}");
                 elementSum += element[i];
             }
+            // calculate average
             double average = (double)elementSum / consoleWriteLine;
             Console.WriteLine($"\nThe Average is {average:0.00}\n");
+            
+            // display difference
             for (int i = 0; i < element.Length; i++)
             {
                 if (average <= element[i])
---
+++ Week 5/Arrayassignments/assignment2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace assignment2
+{
+    internal class Program
+    {
+        const int FixedAmountOfElements = 20;
+        const int MaximumValue = 149;
+        const int MinimumValue = 0;
+        static void Main(string[] args)
+        {
+            int[] element = new int[FixedAmountOfElements]; // initialize array
+            for (int i = 0; i < element.Length; i++)
+            {
+                element[i] = new Random().Next(MinimumValue, MaximumValue); // randomize from 0 to 149
+                Console.WriteLine($"Element {i} = {element[i]}");
+            }
+            int minimum = element[0];
+            for (int i = 0; i < element.Length; i++) // find minimum value
+            {
+                if (element[i] < minimum)
+                {
+                    minimum = element[i];
+                }
+
+            }
+            int lowestValueCount = 0;
+            for (int i = 0; i < element.Length; i++)
+            {
+                if (element[i] == minimum)
+                {
+                    lowestValueCount++;
+                }
+            }
+            Console.WriteLine($"\nSmallest number = {minimum}");
+            Console.WriteLine($"Number of occurence = {lowestValueCount}");
+        }  
+    }
+}
