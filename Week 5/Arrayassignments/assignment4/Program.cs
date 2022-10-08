c7608b116ce6adcebf4833bbaa3677392c60041b
TheLucas27 <Lucemi_cheung@hotmail.com>
2022-10-08 19:20:10 +0200

    finished assignment 4

---
+++ Week 5/Arrayassignments/assignment4/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace assignment4
+{
+    internal class Program
+    {
+        const int MaxInputs = 19;
+        static void Main(string[] args)
+        {
+            // initialize array
+            int[] allInputs = new int[MaxInputs];
+
+            // display 20 numbers
+            for (int i = 0; i < allInputs.Length; i++)
+                {
+                    Console.Write("Enter a number (0=stop): ");
+                    allInputs[i] = int.Parse(Console.ReadLine());
+                if (allInputs[i] == 0)
+                    {
+                    break;
+                    }  
+                }
+            Console.Write("\nEnter a searchvalue: ");
+            int searchValue = int.Parse(Console.ReadLine());
+            int consoleWrites = 0;
+            for (int i = 0; i < allInputs.Length; i++)
+                if (searchValue == allInputs[i])
+                {
+                    consoleWrites++;
+                }
+            Console.Write($"Number of occurences of searchvalue ({searchValue}) is: {consoleWrites}");
+        }
+    }
+}
