8e66966c43f029ce87dfd44163f7de510d0b77b3
TheLucas27 <Lucemi_cheung@hotmail.com>
2022-10-10 16:38:43 +0200

    finished assignment 1

---
+++ Week 6/Methodassignments/assignment1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace assignment1
+{
+    internal class Program
+    {
+        const double VatRate = 0.21;
+        static void Main(string[] args)
+        {
+            // set program of culture
+            CultureInfo ci = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+
+
+            Console.Write("Enter a price: ");
+            double price = double.Parse(Console.ReadLine());
+            float vatPrice, total;
+            VatAndTotal(VatRate, price, out vatPrice, out total);
+            Console.Write($"price: {price:0.00}, VAT: {vatPrice:0.00}, total: {total:0.00}");
+        }
+
+
+        static void VatAndTotal(double VatRate, double price, out float vatPrice, out float total)
+        {
+            vatPrice = (float)price * (float)VatRate;
+            total = (float)price + (float)vatPrice;
+        }
+    }
+}
