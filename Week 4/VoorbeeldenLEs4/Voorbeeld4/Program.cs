2c43d7ebdeb567f3f5c31d6d0dde10b1dd1bd248
TheLucas27 <Lucemi_cheung@hotmail.com>
2022-10-17 00:37:20 +0200

    worked on assignment 2

--- Week 4/VoorbeeldenLEs4/Voorbeeld4/Program.cs
+++ Week 4/VoorbeeldenLEs4/Voorbeeld4/Program.cs
@@ -4,15 +4,28 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 10; i++)
+            int number = int.Parse(Console.ReadLine());
+            bool isPrime = true;
+            int i = 2;
+            while ((i < number) && isPrime)
             {
-                for (int j = 0; j <= 10; j++)
+                if ((number % i) == 0)
                 {
-                    Console.Write("*");
+                    isPrime = false;
                 }
-
-              Console.WriteLine();
-            } 
+                else
+                {
+                    i += 1;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine($"{number} is a prime number");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not a prime number");
+            }
         }
     }
 }
--- Week 6/Methodassignments/assignment2/Program.cs
+++ Week 6/Methodassignments/assignment2/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int primeNumberCalculation = 0;
+            bool primeNumber = false;
+            while (!primeNumber)
+            {
+                Console.Write("Enter Number (0 is stop value): ");
+                primeNumberCalculation = int.Parse(Console.ReadLine());
+                if (primeNumberCalculation == 0)
+
+                {
+                    Console.WriteLine("end of program");
+                    primeNumber = true;
+                }
+                else if (primeNumberCalculation < 0)
+                {
+                    Console.WriteLine("Negative number entered...");
+                }
+                else if (primeNumberCalculation > 0)
+                {
+                    if (!primeNumber)
+                    {
+                        Console.WriteLine($"{primeNumberCalculation} is a prime number.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{primeNumberCalculation} is not a prime number.");
+                    }
+                }
+            }
+
+
+
+
+        }
+        static bool IsPrimeNumber(int primeNumberCalculation)
+        {
+            bool primeNumber = false;
+            int i = 2;
+            while  ((i < primeNumberCalculation) && !primeNumber)
+            {
+                if ((primeNumberCalculation % i) == 0)
+                {
+                    primeNumber = true;
+                }
+                else
+                {
+                    i += 1;
+                }
+            } 
+            return primeNumber;
         }
     }
 }
